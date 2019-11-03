478822b1f4f3655109b4a78eea28d022629f947d
unknown <cj-ms1@hotmail.com>
2019-11-03 19:10:55 +0100

    consulta,lista de usuarios

---
+++ cliente/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IP = new System.Windows.Forms.TextBox();
+            this.nombre = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Longitud = new System.Windows.Forms.RadioButton();
+            this.Bonito = new System.Windows.Forms.RadioButton();
+            this.Desconectar = new System.Windows.Forms.Button();
+            this.Registrarse = new System.Windows.Forms.Button();
+            this.Loguearse = new System.Windows.Forms.Button();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.txtReContraseña = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.Usuario = new System.Windows.Forms.Label();
+            this.Contraseña = new System.Windows.Forms.Label();
+            this.RepetirContraseña = new System.Windows.Forms.Label();
+            this.User = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.btn_mostrar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // IP
+            // 
+            this.IP.Location = new System.Drawing.Point(84, 52);
+            this.IP.Name = "IP";
+            this.IP.Size = new System.Drawing.Size(130, 20);
+            this.IP.TabIndex = 2;
+            this.IP.Text = "192.168.56.102";
+            // 
+            // nombre
+            // 
+            this.nombre.Location = new System.Drawing.Point(116, 31);
+            this.nombre.Name = "nombre";
+            this.nombre.Size = new System.Drawing.Size(164, 20);
+            this.nombre.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(287, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "conectar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(131, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Enviar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.groupBox1.Controls.Add(this.Longitud);
+            this.groupBox1.Controls.Add(this.Bonito);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.nombre);
+            this.groupBox1.Location = new System.Drawing.Point(8, 207);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(363, 202);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Peticion";
+            // 
+            // Longitud
+            // 
+            this.Longitud.AutoSize = true;
+            this.Longitud.Location = new System.Drawing.Point(116, 91);
+            this.Longitud.Name = "Longitud";
+            this.Longitud.Size = new System.Drawing.Size(166, 17);
+            this.Longitud.TabIndex = 7;
+            this.Longitud.TabStop = true;
+            this.Longitud.Text = "Dime la longitud de mi nombre";
+            this.Longitud.UseVisualStyleBackColor = true;
+            // 
+            // Bonito
+            // 
+            this.Bonito.AutoSize = true;
+            this.Bonito.Location = new System.Drawing.Point(116, 68);
+            this.Bonito.Name = "Bonito";
+            this.Bonito.Size = new System.Drawing.Size(156, 17);
+            this.Bonito.TabIndex = 8;
+            this.Bonito.TabStop = true;
+            this.Bonito.Text = "Dime si mi nombre es bonito";
+            this.Bonito.UseVisualStyleBackColor = true;
+            // 
+            // Desconectar
+            // 
+            this.Desconectar.Location = new System.Drawing.Point(463, 298);
+            this.Desconectar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Desconectar.Name = "Desconectar";
+            this.Desconectar.Size = new System.Drawing.Size(132, 40);
+            this.Desconectar.TabIndex = 7;
+            this.Desconectar.Text = "Desconectar";
+            this.Desconectar.UseVisualStyleBackColor = true;
+            this.Desconectar.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Registrarse
+            // 
+            this.Registrarse.Location = new System.Drawing.Point(463, 96);
+            this.Registrarse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Registrarse.Name = "Registrarse";
+            this.Registrarse.Size = new System.Drawing.Size(91, 35);
+            this.Registrarse.TabIndex = 8;
+            this.Registrarse.Text = "Registrarse";
+            this.Registrarse.UseVisualStyleBackColor = true;
+            this.Registrarse.Click += new System.EventHandler(this.Registrarse_Click);
+            // 
+            // Loguearse
+            // 
+            this.Loguearse.Location = new System.Drawing.Point(463, 228);
+            this.Loguearse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Loguearse.Name = "Loguearse";
+            this.Loguearse.Size = new System.Drawing.Size(91, 36);
+            this.Loguearse.TabIndex = 9;
+            this.Loguearse.Text = "Loguearse";
+            this.Loguearse.UseVisualStyleBackColor = true;
+            this.Loguearse.Click += new System.EventHandler(this.Loguearse_Click);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(463, 22);
+            this.txtUsuario.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(68, 20);
+            this.txtUsuario.TabIndex = 10;
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(463, 60);
+            this.txtContraseña.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(68, 20);
+            this.txtContraseña.TabIndex = 11;
+            // 
+            // txtReContraseña
+            // 
+            this.txtReContraseña.Location = new System.Drawing.Point(557, 60);
+            this.txtReContraseña.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtReContraseña.Name = "txtReContraseña";
+            this.txtReContraseña.Size = new System.Drawing.Size(68, 20);
+            this.txtReContraseña.TabIndex = 12;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(463, 162);
+            this.txtUser.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(68, 20);
+            this.txtUser.TabIndex = 13;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(463, 196);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(68, 20);
+            this.txtPassword.TabIndex = 14;
+            // 
+            // Usuario
+            // 
+            this.Usuario.AutoSize = true;
+            this.Usuario.Location = new System.Drawing.Point(460, 6);
+            this.Usuario.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Usuario.Name = "Usuario";
+            this.Usuario.Size = new System.Drawing.Size(43, 13);
+            this.Usuario.TabIndex = 15;
+            this.Usuario.Text = "Usuario";
+            // 
+            // Contraseña
+            // 
+            this.Contraseña.AutoSize = true;
+            this.Contraseña.Location = new System.Drawing.Point(460, 45);
+            this.Contraseña.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Contraseña.Name = "Contraseña";
+            this.Contraseña.Size = new System.Drawing.Size(61, 13);
+            this.Contraseña.TabIndex = 16;
+            this.Contraseña.Text = "Contraseña";
+            // 
+            // RepetirContraseña
+            // 
+            this.RepetirContraseña.AutoSize = true;
+            this.RepetirContraseña.Location = new System.Drawing.Point(555, 45);
+            this.RepetirContraseña.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.RepetirContraseña.Name = "RepetirContraseña";
+            this.RepetirContraseña.Size = new System.Drawing.Size(95, 13);
+            this.RepetirContraseña.TabIndex = 17;
+            this.RepetirContraseña.Text = "RepetirContraseña";
+            // 
+            // User
+            // 
+            this.User.AutoSize = true;
+            this.User.Location = new System.Drawing.Point(463, 148);
+            this.User.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.User.Name = "User";
+            this.User.Size = new System.Drawing.Size(29, 13);
+            this.User.TabIndex = 18;
+            this.User.Text = "User";
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Location = new System.Drawing.Point(462, 181);
+            this.Password.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(53, 13);
+            this.Password.TabIndex = 19;
+            this.Password.Text = "Password";
+            // 
+            // btn_mostrar
+            // 
+            this.btn_mostrar.Location = new System.Drawing.Point(639, 182);
+            this.btn_mostrar.Name = "btn_mostrar";
+            this.btn_mostrar.Size = new System.Drawing.Size(75, 23);
+            this.btn_mostrar.TabIndex = 20;
+            this.btn_mostrar.Text = "Mostrar";
+            this.btn_mostrar.UseVisualStyleBackColor = true;
+            this.btn_mostrar.Click += new System.EventHandler(this.btn_mostrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 487);
+            this.Controls.Add(this.btn_mostrar);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.User);
+            this.Controls.Add(this.RepetirContraseña);
+            this.Controls.Add(this.Contraseña);
+            this.Controls.Add(this.Usuario);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtReContraseña);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.Loguearse);
+            this.Controls.Add(this.Registrarse);
+            this.Controls.Add(this.Desconectar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.IP);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox IP;
+        private System.Windows.Forms.TextBox nombre;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton Longitud;
+        private System.Windows.Forms.RadioButton Bonito;
+        private System.Windows.Forms.Button Desconectar;
+        private System.Windows.Forms.Button Registrarse;
+        private System.Windows.Forms.Button Loguearse;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.TextBox txtReContraseña;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label Usuario;
+        private System.Windows.Forms.Label Contraseña;
+        private System.Windows.Forms.Label RepetirContraseña;
+        private System.Windows.Forms.Label User;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.Button btn_mostrar;
+    }
+}
+
---
+++ cliente/WindowsFormsApplication1/Form2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form2 : Form
+    {
+        Socket server;
+        public Form2()
+        {
+            InitializeComponent();
+            //Creamos un IPEndPoint con el ip del servidor y puerto del servidor 
+            //al que deseamos conectarnos
+            IPAddress direc = IPAddress.Parse("192.168.56.101");
+            IPEndPoint ipep = new IPEndPoint(direc, 9050);
+
+
+            //Creamos el socket 
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                server.Connect(ipep);//Intentamos conectar el socket
+                this.BackColor = Color.Green;
+                MessageBox.Show("Conectado");
+
+            }
+            catch (SocketException ex)
+            {
+                //Si hay excepcion imprimimos error y salimos del programa con return 
+                MessageBox.Show("No he podido conectar con el servidor");
+                return;
+            }
+
+        }
+        public void setServer(Socket serv)
+        {
+            //this.server = serv;
+        }
+
+        private void btn_mos_jug_Click(object sender, EventArgs e)
+        {
+            string mensaje = "13/entrar";
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+
+            //Recibimos la respuesta del servidor
+            byte[] msg2 = new byte[80];
+            server.Receive(msg2);
+            mensaje = Encoding.ASCII.GetString(msg2).Split(',')[0];
+            MessageBox.Show(mensaje);
+        }
+    }
+}
